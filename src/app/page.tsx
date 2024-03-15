3a6efa1682545f1449747dc2756c73df681f1795
freetimezp <kraftukrnet@ukr.net>
2024-03-15 20:34:49 +0200

    03. js-portfolio-59-card. Animation Layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <>
       <Header />
+
+      <div style={{ height: '500vh' }}>
+
+      </div>
     </>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,36 +5,37 @@ import Link from "next/link";
 import { RiMenu3Line } from "react-icons/ri";
 
 import { dataHeader } from "./Header.data";
+import { MotionTransition } from "../MotionTransition/MotionTransition";
 
 export function Header() {
     const [openMenu, setOpenMenu] = useState(false);
 
-
-
     return (
-        <nav className="flex flex-wrap items-center justify-between max-w-5xl
+        <MotionTransition>
+            <nav className="flex flex-wrap items-center justify-between max-w-5xl
             p-4 mx-auto md:py-8">
-            <Link href="/" className="flex items-center">
-                <Image src="/assets/logo.png" alt="logo" width="150" height="40" />
-            </Link>
+                <Link href="/" className="flex items-center">
+                    <Image src="/assets/logo.png" alt="logo" width="150" height="40" />
+                </Link>
 
-            <RiMenu3Line
-                className="block text-3xl md:hidden cursor-pointer"
-                onClick={() => setOpenMenu(!openMenu)}
-            />
+                <RiMenu3Line
+                    className="block text-3xl md:hidden cursor-pointer"
+                    onClick={() => setOpenMenu(!openMenu)}
+                />
 
-            <div className={`${openMenu ? 'block' : 'hidden'} w-full md:block md:w-auto`}>
-                <div className="flex flex-col p-4 mt-4 md:p-0 md:flex-row md:space-x-8
+                <div className={`${openMenu ? 'block' : 'hidden'} w-full md:block md:w-auto`}>
+                    <div className="flex flex-col p-4 mt-4 md:p-0 md:flex-row md:space-x-8
                     md:mt-0 md:border-0">
-                    {dataHeader.map(({ id, name, idLink }) => (
-                        <div key={id} className="px-4 transition-all duration-500 ease-in-out">
-                            <Link href={idLink} className="text-lg hover:text-secondary">
-                                {name}
-                            </Link>
-                        </div>
-                    ))}
+                        {dataHeader.map(({ id, name, idLink }) => (
+                            <div key={id} className="px-4 transition-all duration-500 ease-in-out">
+                                <Link href={idLink} className="text-lg hover:text-secondary">
+                                    {name}
+                                </Link>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </MotionTransition>
     )
 }
