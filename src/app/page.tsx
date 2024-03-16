ec24f6dea207950b3351632190bf9afae13f7a2b
freetimezp <kraftukrnet@ukr.net>
2024-03-16 12:05:42 +0200

    06. js-portfolio-59-card. CounterData component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Header } from "@/components/Header";
 import FirstBlock from "@/components/FirstBlock/FirstBlock";
+import CounterData from "@/components/CounterData/CounterData";
 
 export default function Home() {
   return (
     <>
       <Header />
       <FirstBlock />
-
+      <CounterData />
     </>
   )
 }
---
+++ src/components/CounterData/CounterData.data.ts
@@ -0,0 +1,20 @@
+export const dataCounter = [
+    {
+        id: 1,
+        startNumber: 500,
+        endNumber: 3857,
+        text: 'active users',
+    },
+    {
+        id: 2,
+        startNumber: 0,
+        endNumber: 235,
+        text: 'employees',
+    },
+    {
+        id: 3,
+        startNumber: 600,
+        endNumber: 1895,
+        text: 'millions invested',
+    },
+];
